8e45a421ff0966556244972df5953d73048b9131
Gustavo Morfim <gustavomorfim@hotmail.com>
2019-08-01 19:04:42 -0300

    modify for accepts object Array

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 module.exports = function converter(list, dic) {
-    if (!dic)
-        throw new Error("invalid dic")
-
+    if (!dic) throw new Error('invalid dic');
+  
     return list.map(item => {
-        const converted = {};
-        Object.keys(item).forEach(x => {
-            if (!dic[x]) return;
-
-            if (typeof dic[x] === 'string') {
-                converted[dic[x]] = item[x];
-                return;
-            }
-            const { name, parser } = dic[x];
-
+      const converted = {};
+      Object.keys(item).forEach(x => {
+        if (!dic[x]) return;
+  
+        if (typeof dic[x] === 'string') {
+          converted[dic[x]] = item[x];
+          return;
+        }
+  
+        if (Object.prototype.toString.call(dic[x]) === '[object Array]') {
+          dic[x].map(y => {
+            const { name, parser } = y;
+  
             converted[name] = parser(item[x], item);
-        });
-        return converted;
+          });
+  
+          return;
+        }
+  
+        const { name, parser } = dic[x];
+  
+        converted[name] = parser(item[x], item);
+      });
+      return converted;
     });
-}
+  };
+  
